64bc5f5954d427e1d058a6c320919bfc8f95a0fe
Ravi Kumar <ravikumar.ghr@gmail.com>
2022-12-31 11:17:07 +0200

    Update GHRConstants.java

--- src/com/ghrartifacts/utilities/GHRConstants.java
+++ src/com/ghrartifacts/utilities/GHRConstants.java
@@ -6,5 +6,8 @@ public class GHRConstants {
 	static String SAVIYNT_HOME = System.getenv("SAVIYNT_HOME");
 	public static String LOG_LEVEL = "GHR_LOG_LEVEL";
 	public static String DATE_FORMAT_FOR_EMAIL_NOTIFICATIONS = "GHR_DATE_FORMAT_FOR_EMAIL_NOTIFICATIONS";
+	// add these properties to externalconfig.properties file by Navigating Admin => Settings => Configuration Files, restart the application servers
+	//GHR_LOG_LEVEL=INFO
+        //GHR_DATE_FORMAT_FOR_EMAIL_NOTIFICATIONS=dd-MM-YYYY
 
 }
